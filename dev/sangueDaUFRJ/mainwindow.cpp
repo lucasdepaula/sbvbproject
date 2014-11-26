a5b2018e3e58203a71f1e190cbd520291a09fd29
Maudemick <thiagoluismaia@poli.ufrj.br>
2014-11-26 10:06:31 -0200

    Trying to use db from Resources

--- dev/sangueDaUFRJ/mainwindow.cpp
+++ dev/sangueDaUFRJ/mainwindow.cpp
@@ -16,8 +16,11 @@ MainWindow::MainWindow(QWidget *parent) :
     ui->lineEdit_2->setEnabled(false);
     setStyleSheet("background-image: url(:/image/data/sangue.png);font-family : Arial, Helvetica, 'Nimbus Sans L'', 'Liberation Sans'', FreeSans, Sans-serif; font-size:13px");
     mydb = QSqlDatabase::addDatabase("QSQLITE");
-    mydb.setDatabaseName("SangueDB.db");
+    mydb.setDatabaseName(":/database/data/sangueDB.db");
     mydb.open();   // colocar mydb como membro de MainWindow
+    if (mydb.open()){
+        qDebug()<<"Hello";
+    }
 }
 
 MainWindow::~MainWindow()
