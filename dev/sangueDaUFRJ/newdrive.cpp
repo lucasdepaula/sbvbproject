38195c00a4a9f73449857216c6d207c6a7b65048
Rafael Damasceno <damscn@gmail.com>
2014-11-27 11:07:51 -0200

    Refresh newdrive
    Auto Current Date and Buttons function

--- dev/sangueDaUFRJ/newdrive.cpp
+++ dev/sangueDaUFRJ/newdrive.cpp
@@ -10,6 +10,7 @@ NewDrive::NewDrive(QWidget *parent) :
     ui(new Ui::NewDrive)
 {
     ui->setupUi(this);
+    ui->dateEdit->setDate(QDate::currentDate());
     setStyleSheet("background-image: url(:/image/data/sangue.png);font-family : Arial, Helvetica, 'Nimbus Sans L'', 'Liberation Sans'', FreeSans, Sans-serif; font-size:13px");
 }
 
@@ -49,4 +50,27 @@ void NewDrive::saveBloodDriveInfo(){
 //    ui->listWidget->;
 
 //    QSqlQuery qry("INSERT INTO BloodDrive (name) VALUES ('"+name+"'');",mydb);
+}
+
+//saves and goes back to mainmenu
+void NewDrive::on_pushButton_3_clicked() //"Salvar e finalizar"
+{
+    //saveBloodDriveInfo()
+    this->close();
+}
+
+//saves and opens a new drive window
+void NewDrive::on_pushButton_2_clicked() //"Salvar e criar próximo"
+{
+    //saveBloodDriveInfo();
+    ui->lineEdit->clear();
+    ui->dateEdit->setDate(QDate::currentDate());
+    ui->spinBox->setValue(10);
+    ui->spinBox_2->setValue(0);
+    ui->spinBox_3->setValue(10);
+    ui->spinBox_4->setValue(0);
+    ui->spinBox_5->setValue(7);
+    ui->spinBox_6->setValue(0);
+    ui->listWidget->clear();
+    return;
 }
--- dev/sangueDaUFRJ/newdrive.h
+++ dev/sangueDaUFRJ/newdrive.h
@@ -26,6 +26,10 @@ private slots:
 
     void on_pushButton_4_clicked();
 
+    void on_pushButton_3_clicked();
+
+    void on_pushButton_2_clicked();
+
 private:
     Ui::NewDrive *ui;
     QMainWindow *appmenu;
