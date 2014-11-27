72a77cac6a3217c1f035fea54bf7a69495bcb56d
IgorRocha <igor.rocha@poli.ufrj.br>
2014-11-27 01:29:40 -0200

    Database created, compound primary keys not working
    Qt creates the database, but setting up compound primary keys is not
    working. The table Major was successfully populated

--- dev/sangueDaUFRJ/mainmenu.cpp
+++ dev/sangueDaUFRJ/mainmenu.cpp
@@ -3,30 +3,30 @@
 #include "mainwindow.h"
 #include <QDesktopWidget>
 
-#define M0 BCMT
-#define M1 Ciência da Computação
-#define M2 Engenharia Ambiental
-#define M3 Engenharia Civil
-#define M4 Engenharia de Alimentos
-#define M5 Engenharia de Bioprocessos
-#define M6 Engenharia de Computação e Informação
-#define M7 Engenharia de Controle e Automação
-#define M8 Engenharia de Materiais
-#define M9 Engenharia de Petróleo
-#define M10 Engenharia de Produção
-#define M11 Engenharia Elétrica
-#define M12 Engenharia Eletrônica e de Computação
-#define M13 Engenharia Mecânica
-#define M14 Engenharia Metalúrgica
-#define M15 Engenharia Naval e Oceânica
-#define M16 Engenharia Nuclear
-#define M17 Engenharia Química
-#define M18 Engenharia (Ciclo Básico)
-#define M19 Física
-#define M20 Matemática
-#define M21 Química
-#define M22 Química Industrial
-#define M23 Outro
+#define M0 "BCMT"
+#define M1 "Ciência da Computação"
+#define M2 "Engenharia Ambiental"
+#define M3 "Engenharia Civil"
+#define M4 "Engenharia de Alimentos"
+#define M5 "Engenharia de Bioprocessos"
+#define M6 "Engenharia de Computação e Informação"
+#define M7 "Engenharia de Controle e Automação"
+#define M8 "Engenharia de Materiais"
+#define M9 "Engenharia de Petróleo"
+#define M10 "Engenharia de Produção"
+#define M11 "Engenharia Elétrica"
+#define M12 "Engenharia Eletrônica e de Computação"
+#define M13 "Engenharia Mecânica"
+#define M14 "Engenharia Metalúrgica"
+#define M15 "Engenharia Naval e Oceânica"
+#define M16 "Engenharia Nuclear"
+#define M17 "Engenharia Química"
+#define M18 "Engenharia (Ciclo Básico)"
+#define M19 "Física"
+#define M20 "Matemática"
+#define M21 "Química"
+#define M22 "Química Industrial"
+#define M23 "Outro"
 
 
 MainMenu::MainMenu(QWidget *parent) :
@@ -96,13 +96,14 @@ void MainMenu::on_pushButton_4_clicked()
 void MainMenu::createDatabase(){
     mydb = QSqlDatabase::addDatabase("QSQLITE");
     mydb.setDatabaseName("sangueDB.db");
+    mydb.open();
     QSqlQuery qry1("CREATE TABLE Donor (id INTEGER PRIMARY KEY AUTOINCREMENT ,majorID INTEGER,name TEXT,email TEXT,phone TEXT DEFAULT (null) ,semester TEXT DEFAULT (null) ,obs TEXT DEFAULT (null) );",mydb);
     QSqlQuery qry2("CREATE TABLE BloodDrive (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, startDate TEXT);",mydb);
     QSqlQuery qry3("CREATE TABLE DonationTime (id INTEGER AUTOINCREMENT, date TEXT NOT NULL, time TEXT NOT NULL, maxDonors INTEGER DEFAULT (0), PRIMARY KEY (id, date, time));",mydb);
     QSqlQuery qry4("CREATE TABLE BloodDriveDonor (id INTEGER AUTOINCREMENT, bloodDriveID INTEGER NOT NULL, donorID INTEGER NOT NULL, PRIMARY KEY (id, bloodDriveID, donorID));",mydb);
-    QSqlQuery qry5("CREATE TABLE Major (id INTEGER AUTOINCREMENT, listedName TEXT NOT NULL, inputName TEXT DEFAULT NULL, PRIMARY KEY(id));",mydb);
+    QSqlQuery qry5("CREATE TABLE Major (id INTEGER PRIMARY KEY AUTOINCREMENT, listedName TEXT NOT NULL, inputName TEXT DEFAULT NULL);",mydb);
 }
 
 void MainMenu::populateMajors(){
-    QSqlQuery qry("INSERT INTO Major (listedName) VALUES ('"+M0+"'),('"+M1+"'),('"+M2+"'),('"+M3+"'),('"+M4+"'),('"+M5+"'),('"+M6+"'),('"+M7+"'),('"+M8+"'),('"+M9+"'),('"+M10+"'),('"+M11+"'),('"+M12+"'),('"+M13+"'),('"+M14+"'),('"+M15+"'),('"+M16+"'),('"+M17+"'),('"+M18+"'),('"+M19+"'),('"+M20+"'),('"+M21+"'),('"+M22+"');",mydb);
+    QSqlQuery qry("INSERT INTO Major (listedName) VALUES ('"M0"'),('"M1"'),('"M2"'),('"M3"'),('"M4"'),('"M5"'),('"M6"'),('"M7"'),('"M8"'),('"M9"'),('"M10"'),('"M11"'),('"M12"'),('"M13"'),('"M14"'),('"M15"'),('"M16"'),('"M17"'),('"M18"'),('"M19"'),('"M20"'),('"M21"'),('"M22"');",mydb);
 }
